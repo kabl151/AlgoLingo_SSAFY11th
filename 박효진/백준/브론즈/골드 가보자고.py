22bcecc3a14a3a2a34732a4da1e9022c4de8859a
강상찬 <tkdcks7@gmail.com>
2024-01-23 13:31:54 +0900

    123123

--- 박효진/백준/브론즈/골드 가보자고.py
+++ 박효진/백준/브론즈/골드 가보자고.py
@@ -1,1 +1,4 @@
 print("아직은 브론즈 2다..")
+
+
+dfsdfasdf
--- 박효진/백준/브론즈/실습 연습장.py
+++ 박효진/백준/브론즈/실습 연습장.py
@@ -1,3 +1,8 @@
-a = hash(2)
-b = hash(2)
-print(a == b)
+# 아래 함수를 수정하시오.
+def remove_duplicates_to_set(num_list):
+    result = set(num_list)
+    return result
+
+
+result = remove_duplicates_to_set([1, 2, 2, 3, 4, 4, 5])
+print(result)
