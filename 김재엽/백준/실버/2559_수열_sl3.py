c02c9e1280e539e73f48a44c3404e74368db1990
Muvinn <kbvi45@gmail.com>
2024-01-28 01:23:38 +0900

    2559_[solved]

--- 김재엽/백준/실버/2559_수열_sl3.py
+++ 김재엽/백준/실버/2559_수열_sl3.py
@@ -1,16 +1,45 @@
-n,m=map(int,input().split())   # n: 입력받을 정수의 개수, m: 연속된 구간의 크기 (10, 5)
-arr=list(map(int,input().split()))
+import sys
+input = sys.stdin.readline
 
-Max,sum=0,0
+n, m = map(int, input().split())
+temperatureArr = list(map(int, input().split()))
 
-for i in range(m): # 처음 m개의 구간 (0번 인덱스부터 4번인덱스)의 합 구하기
-    sum+=arr[i]
 
-for i in range(n-m+1): 
-    if sum>Max:
-        Max=sum
-    if i==n-m: break
-    sum+=arr[i+m] # i+m: 5~10
-    sum-=arr[i]   # i: 0~5
+#누적합을 구하는 방식으로 접근할 예정
+preArr = [0]
+tmp_sum = []
+mysum = 0
 
-print(Max)
+#누적합을 쌓을 리스트 : mysum
+for i in range(n):
+    mysum += temperatureArr[i]
+    preArr.append(mysum)
+
+
+for i in range(n-m+1):
+    tmp_sum.append(preArr[i+m] - preArr[i])
+
+#단위별 온도 합계 쌓인 리스트에서 max() 사용하여 최댓값 산출
+print(max(tmp_sum))
+
+
+
+
+
+"""
+import sys
+#sys.stdin.readline()
+#N은 전체 날 수, K는 연속적인 날짜 수
+N, K = map(int, sys.stdin.readline().split())
+temperatureArr = list(map(int, sys.stdin.readline().split()))
+maxValue = temperatureArr[0]
+
+for i in range(N-(K-1)):
+    tempValue = 0
+    for k in range(K):
+        tempValue += temperatureArr[i+k]
+    if tempValue >= maxValue:
+        maxValue = tempValue
+        
+print(maxValue)
+"""
