1023e384bbc30a7bee32377157185b4456d2f40f
hyojin <cron.phj@gmail.com>
2024-02-15 22:40:41 +0900

    머리아팡

--- 박효진/SWEA/미로.py
+++ 박효진/SWEA/미로.py
@@ -44,7 +44,7 @@ for tc in range(1, T+1):
     visited = [[False]*N for _ in range(N)]
     maze = [list(map(int, input().rstrip())) for _ in range(N)]
     result = 0
-    # 상, 하, 좌, 우 방향을 나타내는 리스트
+    cnt = 0
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     for i in range(N):
@@ -55,17 +55,23 @@ for tc in range(1, T+1):
                 ei, ej = i, j
     # DFS 함수 정의
     def DFS(x, y):
-        visited[x][y] = True # 현재 위치를 방문 처리
         global result
+        global cnt
+        # if visited [x][y] == 0: 
+        #     cnt += 1
+        visited[x][y] = True # 현재 위치를 방문 처리
         # 네 방향을 확인
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            # 미로 범위 내에 있고, 아직 방문하지 않은 위치라면
             if nx >= 0 and nx < len(maze) and ny >= 0 and ny < len(maze[0]) and maze[nx][ny] == 0 and visited[nx][ny] == False:
+                cnt +=1
                 DFS(nx, ny) # DFS로 이동
             elif nx >= 0 and nx < len(maze) and ny >= 0 and ny < len(maze[0]) and maze[nx][ny] == 2:
                 result = 1
         return result
 
     DFS(si, sj)
-    print(f'#{tc} {result}')
+    if result == 1:
+        print(f'#{tc} {cnt}')
+    else:
+        print(f'#{tc}', 0)
--- 박효진/SWEA/정곤이.py
+++ 박효진/SWEA/정곤이.py
@@ -1,30 +1,25 @@
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())
-    arr = list(map(int, input().split()))
+    N = int(input().rstrip())
+    arr = list(map(int, input().rstrip().split()))
     lst = []
     result = []
     for i in range(N):
         for j in range(i+1, N):
             lst.append(str(arr[i] * arr[j]))
     for i in range(len(lst)):
-        
-
-
-
-
-    # for i in range(len(lst)):
-    #     if len(lst[i]) >= 2:
-    #         jd = 1
-    #         for j in range(1, int(lst[i])):
-    #             if int(lst[i][j+1]) < int(lst[i][j-1]):
-    #                 jd = 0
-    #             else:
-    #                 pass
-    #         if jd == 1:
-    #             result.append(i)
-    #         else:
-    #             pass
-    #     else:
-    #         result.append(i)
-    # print(result)
+        if len(lst[i]) == 1:
+            result.append(int(lst[i]))
+        else:
+            result_num = 1
+            for j in range(len(lst[i])-1):
+                if int(lst[i][j]) <= int(lst[i][j+1]):
+                    continue
+                else:
+                    result_num = 0
+            if result_num == 1:
+                result.append(int(lst[i]))
+    if len(result) == 0:
+        print(f'#{tc}', -1)
+    else:
+        print(f'#{tc}', max(result))
